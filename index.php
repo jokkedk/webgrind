96783d657de0d999c17b15dac42af194d3c20268
Iulian Onofrei <revolt666@users.noreply.github.com>
2014-07-07 13:30:50 +0300

    added quotes around file paths

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ try {
             header("Content-Type: image/png");
             $filename = Webgrind_Config::storageDir().$dataFile.'-'.$showFraction.Webgrind_Config::$preprocessedSuffix.'.png';
 		    if (!file_exists($filename)) {
-				shell_exec(Webgrind_Config::$pythonExecutable.' library/gprof2dot.py -n '.$showFraction.' -f callgrind '.Webgrind_Config::xdebugOutputDir().''.$dataFile.' | '.Webgrind_Config::$dotExecutable.' -Tpng -o ' . $filename);
+				shell_exec(Webgrind_Config::$pythonExecutable.' library/gprof2dot.py -n '.$showFraction.' -f callgrind "'.Webgrind_Config::xdebugOutputDir().''.$dataFile.'" | '.Webgrind_Config::$dotExecutable.' -Tpng -o "' . $filename . '"');
 			}
 			readfile($filename);
 		break;
